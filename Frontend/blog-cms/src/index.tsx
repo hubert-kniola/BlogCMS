205a98db06cfa802cdad6afa3f3e9c1bbed7ff6b
hubert-kniola <hubert.kniola@gmail.com>
2022-10-15 17:45:09 +0200

    redux init

--- Frontend/blog-cms/src/index.tsx
+++ Frontend/blog-cms/src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
