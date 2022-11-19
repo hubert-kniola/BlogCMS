7e3a484d027be8785129895fa72f72910ccb187e
mateuszkuzniak <mateokuz@gmail.com>
2022-11-19 13:28:51 +0100

    refactore slider with MORE SPACE

--- Frontend/blog-cms/src/components/Slider/Slider.tsx
+++ Frontend/blog-cms/src/components/Slider/Slider.tsx
@@ -1,21 +1,8 @@
 import React, { useEffect, useRef, useState } from "react"
-import { BEM, GetTextPositionStyle } from "../../tools"
 import { SlideType } from "../../types"
+import { Slide } from "./Slide"
+import { SliderNavigation } from "./SliderNavigation"
 import "./style.css"
-
-const css = {
-  slider: "slider",
-  allSlides: "allSlides",
-  slide: "slide",
-  container: "container",
-  content: "content",
-  navigation: "navigation",
-  dot:"dot",
-  title: "title",
-  modifiers: {
-    active: "active"
-  }
-}
 
 interface ISlider  {
   slides : SlideType[]
@@ -25,12 +12,11 @@ export const Slider = ({slides} : ISlider) => {
   const [index, setIndex] = useState(0);
   const timeoutRef = useRef(null);
 
-
   const resetTimeout = () => {
-    if(timeoutRef.current){
+    if (timeoutRef.current) {
       clearTimeout(timeoutRef.current);
     }
-  }
+  };
 
   useEffect(() => {
     resetTimeout();
@@ -45,102 +31,39 @@ export const Slider = ({slides} : ISlider) => {
     return () => {
       resetTimeout();
     };
-  }, [index])
+  }, [index]);
 
-    return (
-      <>
-        <div className="slider_container">
-          <div className="view">
-            <div
-              className="slider_row"
-              style={{ transform: `translate3d(${(-index * 100) / slides.length}%, 0, 0)` }}
-            >
-              {
-
-              }
-              {
-                slides.map((item, idx) => {
-                  return (
-                    <div className="slider_slide" key={idx}>
-                      <div
-                        className={
-                          idx === index ? "slide_box--active" : `slide_box`
-                        }
-                        style={{
-                          backgroundImage: `url(${item.photoUrl})`,
-                        }}
-                      ></div>
-                    </div>
-                  );
-                })
-              }
-            </div>
+  return (
+    <>
+      <div className="slider_container">
+        <div className="view">
+          <div
+            className="slider_row"
+            style={{
+              width: `${100 * slides.length}%`,
+              transform: `translate3d(${
+                (-index * 100) / slides.length
+              }%, 0, 0)`,
+            }}
+          >
+            {slides.map((item, idx) => {
+              return (
+                <Slide
+                  slidesLenght={slides.length}
+                  active={index === idx}
+                  idx={idx}
+                  slide={item}
+                />
+              );
+            })}
           </div>
         </div>
-        <div className={BEM(css.slider,css.navigation)}>
-            {slides.map((_, idx) => {
-              return (
-                <div
-                  key={idx}
-                  className={`${BEM(css.slider, css.navigation, css.dot)} ${
-                    idx === index
-                      ? BEM(css.slider, css.navigation, css.modifiers.active)
-                      : ""
-                  }`}
-                  onClick={() => setIndex(idx)}
-                />
-              );
-            })}
-          </div>
-        {/* <div className={BEM(css.slider, css.container)}>
-          <div className={BEM(css.slider,css.allSlides)} style={{width: `${slides.length*100}%`}}>
-            {slides.map((oneSlide, idx) => {
-              return (
-                <>
-                  <div
-                    key={idx}
-                    className={BEM(css.slider, css.slide)}
-                    style={{
-                      backgroundImage: `url(${oneSlide.photoUrl})`,
-                      transform: `translate3d(${-index * 100}%, 0, 0)`,
-                      width: `${100 / slides.length}%`,
-                    }}
-                  >
-                    <div className={BEM(css.slide,css.content)} style={GetTextPositionStyle(oneSlide.textPosition)} >
-                        <p
-                        className={BEM(css.slide,css.content, css.title)}
-                        >
-                          {oneSlide.title}
-                          {oneSlide.content && (
-                            <p
-                            className={BEM(css.slide,css.content, css.content)}
-                            >
-                              {oneSlide.content}
-                            </p>
-                          )}
-                        </p>
-                    </div>
-                  </div>
-                </>
-              );
-            })}
-          </div>
-          <div className={BEM(css.slider,css.navigation)}>
-            {slides.map((_, idx) => {
-              return (
-                <div
-                  key={idx}
-                  className={`${BEM(css.slider, css.navigation, css.dot)} ${
-                    idx === index
-                      ? BEM(css.slider, css.navigation, css.modifiers.active)
-                      : ""
-                  }`}
-                  onClick={() => setIndex(idx)}
-                />
-              );
-            })}
-          </div>
-        </div> */}
-      </>
-    );
+      </div>
+      <SliderNavigation
+        onClickHandler={(idx: number) => setIndex(idx)}
+        activeIdx={index}
+        slides={slides}
+      />
+    </>
+  );
 }
---
+++ Frontend/blog-cms/src/components/Slider/SliderNavigation.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { BEM } from "../../tools";
+import { css } from "./css";
+
+interface ISliderNavigation  {
+    onClickHandler: (idx: number) => void;
+    activeIdx: number;
+    slides: any[];
+}
+
+export const SliderNavigation = ({onClickHandler, activeIdx, slides} : ISliderNavigation) => {
+    
+    return(
+        <div className={BEM(css.slider, css.navigation)}>
+        {slides.map((_, idx) => {
+          return (
+            <div
+              key={idx}
+              className={`${BEM(css.slider, css.navigation, css.dot)} ${
+                idx === activeIdx
+                  ? BEM(css.slider, css.navigation, css.modifiers.active)
+                  : ""
+              }`}
+              onClick={() => onClickHandler(idx)}
+            />
+          );
+        })}
+      </div>
+    )
+}
