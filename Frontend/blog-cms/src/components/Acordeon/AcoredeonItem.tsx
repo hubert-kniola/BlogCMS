b8ff1663f3c8e0023ff3eeab4ed9bfa92bc1a1dd
mateuszkuzniak <mateokuz@gmail.com>
2023-01-05 23:14:30 +0100

    css fix. PLEASE PAY ATTENTION TO CLASS NAMES!!!

--- Frontend/blog-cms/src/components/Acordeon/AcoredeonItem.tsx
+++ Frontend/blog-cms/src/components/Acordeon/AcoredeonItem.tsx
@@ -1,35 +1,51 @@
-import React from "react"
-import { Faq } from "../../types"
-import  "./style.css"
+import React from "react";
+import { BEM } from "../../tools";
+import { Faq } from "../../types";
+import "./style.css";
 
-interface IFaqItem  {
-    setActiveFaq: () => void
-    isActive: boolean
-    faq: Faq
+interface IFaqItem {
+  setActiveFaq: () => void;
+  isActive: boolean;
+  faq: Faq;
 }
 
-export const AcordeonItem = ({setActiveFaq, isActive, faq} : IFaqItem) => {
-    return (
-      <>
-        <div className="faq_item">
-          <div className="faq_item--tilte">
-            <div className="faq_item--question">
-              {faq.question.endsWith("?") ? faq.question : faq.question + "?"}
-            </div>
-            <div className="faq_item--mark" onClick={() => setActiveFaq()}>
-              {isActive ? "-" : "+"}
-            </div>
+const css = {
+  faq: "faqMain",
+  item: "item",
+  title: "tilte",
+  question: "question",
+  mark: "mark",
+  content: "content",
+  active: "active",
+};
+
+export const AcordeonItem = ({ setActiveFaq, isActive, faq }: IFaqItem) => {
+  return (
+    <>
+      <div className={BEM(css.faq, css.item)} onClick={() => setActiveFaq()}>
+        <div className={BEM(css.faq, css.item, css.title)}>
+          <div className={BEM(css.faq, css.item, css.question)}>
+            {faq.question.endsWith("?") ? faq.question : faq.question + "?"}
           </div>
           <div
-            className={`${
-              isActive
-                ? "faq_item--content faq_item--active"
-                : "faq_item--content"
-            }`}
-          >
-            {faq.answer}
+            className={BEM(css.faq, css.item, css.mark)}
+            onClick={() => setActiveFaq()}>
+            {isActive ? "-" : "+"}
           </div>
         </div>
-      </>
-    );
-}
+        <div
+          className={`${
+            isActive
+              ? `${BEM(css.faq, css.item, css.content)} ${BEM(
+                  css.faq,
+                  css.item,
+                  css.active
+                )}`
+              : BEM(css.faq, css.item, css.content)
+          }`}>
+          {faq.answer}
+        </div>
+      </div>
+    </>
+  );
+};
